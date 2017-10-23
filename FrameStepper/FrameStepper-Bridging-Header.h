2cb17dc240d2da88908ced0c961a6d89517a765c
Kazuya Shida <kazuya.4da@gmail.com>
2017-10-23 22:49:47 +0900

    Initializad commit

---
+++ FrameStepper/FrameStepper-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  FrameStepper-Bridging-Header.h
+//  FrameStepper
+//
+//  Created by Kazuya Shida on 2017/10/23.
+//  Copyright © 2017 mani3. All rights reserved.
+//
+
+#ifndef FrameStepper_Bridging_Header_h
+#define FrameStepper_Bridging_Header_h
+
+/// ffmpeg
+#import <libavcodec/avcodec.h>
+#import <libavformat/avformat.h>
+#import <libswscale/swscale.h>
+
+#endif /* FrameStepper_Bridging_Header_h */
---
+++ FrameStepper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FrameStepper
+//
+//  Created by Kazuya Shida on 2017/10/23.
+//  Copyright © 2017 mani3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        av_register_all()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
