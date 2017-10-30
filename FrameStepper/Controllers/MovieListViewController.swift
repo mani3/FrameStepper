5e5c2fb30fdad906c1d8a000ee4f2df0ad3659b4
Kazuya Shida <kazuya.4da@gmail.com>
2017-10-30 22:17:40 +0900

    Add carthage

---
+++ FrameStepper/Controllers/MovieListViewController.swift
@@ -0,0 +1,66 @@
+//
+//  MovieListViewController.swift
+//  FrameStepper
+//
+//  Created by Kazuya Shida on 2017/10/30.
+//  Copyright © 2017 mani3. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+class MovieListViewController: UIViewController {
+    let dispose = DisposeBag()
+
+    @IBOutlet weak var tableView: UITableView!
+
+    fileprivate var files = Variable<[URL]>([])
+
+    let ext = ["mp4", "mov", "avi", "mkv"]
+
+    private var moviePaths: [URL] {
+        let manager = FileManager.default
+        guard let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return []
+        }
+        do {
+            let contents = try manager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+            let files = contents.filter { ext.contains($0.pathExtension) }
+            return files
+        } catch {
+            return []
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        files.asObservable()
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell")) { (_, item: URL, cell) in
+                cell.textLabel?.text = item.lastPathComponent
+                cell.detailTextLabel?.text = item.absoluteString
+            }
+            .disposed(by: dispose)
+
+        tableView.rx.modelSelected(URL.self)
+            .subscribe(onNext: { (_) in
+            })
+            .disposed(by: dispose)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        files.value = moviePaths
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
--- FrameStepper/ViewController.swift
+++ FrameStepper/ViewController.swift
@@ -12,15 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         av_register_all()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        performSegue(withIdentifier: "MovieListSegue", sender: nil)
+    }
 }
-
